dcebca7f811993ead9ae4fe74544002134f6f349
Cade  Mallett <cmallett@boingo.com>
2019-07-03 19:54:05 -0700

    deleted old files

--- client.py
+++
@@ -1,9 +0,0 @@
-from socketIO_client import SocketIO, LoggingNamespace
-
-def on_aaa_response(args):
-    print('on_aaa_response', args['data'])
-
-socketIO = SocketIO('localhost', 8000, LoggingNamespace)
-socketIO.on('aaa_response', on_aaa_response)
-socketIO.emit('aaa')
-socketIO.wait(seconds=1)
