ae3c2fdae29f5e9a2370c2ece2fe50d323fa1057
Cade  Mallett <cmallett@boingo.com>
2019-07-10 21:49:42 -0700

    started html

--- server/web_server.py
+++ server/web_server.py
@@ -8,11 +8,22 @@ app = Flask(__name__)
 
 @app.route("/")
 def login_page():
-    return "Please Authenticate"
+    return render_template("login.html")
+    #return "Please Authenticate"
 
 @app.route("/logged_in")
 def stream_selector():
-    return "View your streams or register a new device"
+    if request.method == 'POST':
+        if request.form['submit_button'] == 'Go to Streams':
+            return "opt1"
+            return redirect(url_for('client_feed'))
+        elif request.form['submit_button'] == 'Register a New Device':
+            return "opt2"
+            return redirect(url_for('register'))
+        else: 
+            return "failed"
+    elif request.method == 'GET':
+        return render_template("logged_in.html")
 
 @app.route("/register_device")
 def register():
@@ -43,12 +54,13 @@ def gen_client(stream_id):
 # stream_id should match with a client name. if that is not the case, then we default to 
 # the server feed 
 #TODO: default should be changed to all streams chosen for the dashboard by the authenticated user
-@app.route('/client_feed')
-def client_feed():
+@app.route('/show_clients')
+def show_clients(stream_id=None):
     #if user not in settings.authenticated_user_list:
     #    return "can't view, log in first"
+    print("s_id", stream_id)
     try:
-        stream_id=request.args['stream_id']
+        #stream_id=request.args['stream_id']
         if not (os.path.isfile('static/' + stream_id + '_frame.jpg') and 
                 stream_id in settings.locks_map):
             raise Exception("not a valid stream_id. default to server feed")
@@ -56,6 +68,23 @@ def client_feed():
                         mimetype='multipart/x-mixed-replace; boundary=frame')
     except: 
         return server_feed()
+        
+def find_all_feeds():
+    feeds = []
+    for i in settings.locks_map:
+        feeds.append(i)
+    print("feeds", feeds)
+    return feeds
+
+@app.route('/client_feed')
+def client_feed():
+    try:
+        f = [ request.args['stream_id'] ]
+    except:
+        f = find_all_feeds()
+        print("finding all feeds")
+    print(f)
+    return render_template('video_feeds.html', feeds=f)
 
 
 def run_web_server():    
